c260327d02cab0d35646c7daaa2fc0917ba928e6
JerezM <jerez149@gmail.com>
2021-10-06 15:19:55 -0300

    Initial commit

---
+++ src/main/java/com/jerezm/seriesflv/SeriesflvApplication.java
@@ -0,0 +1,13 @@
+package com.jerezm.seriesflv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriesflvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeriesflvApplication.class, args);
+	}
+
+}
