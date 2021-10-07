c793f1650523395866d1621555bc1732fb07da86
JerezM <jerez149@gmail.com>
2021-10-07 13:06:36 -0300

    SerieController modified and working well

--- src/main/java/com/jerezm/seriesflv/serie/SerieController.java
+++ src/main/java/com/jerezm/seriesflv/serie/SerieController.java
@@ -3,8 +3,11 @@ package com.jerezm.seriesflv.serie;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,10 +26,14 @@ public class SerieController {
     public ResponseEntity<List<Serie>> getAllSeries(){
         ResponseEntity<List<Serie>> responseEntity;
 
-        List<Serie> serieList = this.serieService.getAllSeries();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
 
-        responseEntity = ResponseEntity.ok().body(serieList);
+        List<Serie> seriesList = this.serieService.getAllSeries();
+
+        responseEntity = new ResponseEntity<List<Serie>>(seriesList, headers, HttpStatus.OK);
 
         return responseEntity;
     }
+
 }
